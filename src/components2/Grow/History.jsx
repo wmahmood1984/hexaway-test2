223bff8c14d4e2628b564128ee64d3698487e7ab
Waqas Mahmood <wmahmood@pakistancables.com>
2026-02-28 10:57:27 +0500

    Update History.jsx

--- src/components2/Grow/History.jsx
+++ src/components2/Grow/History.jsx
@@ -16,9 +16,9 @@ export default function History({
           {statusText==='Active' ?
             <div style={{background:"rgba(16,185,129,0.2)", padding:"8px", borderRadius:"8px", marginBottom:"8px"}}>
                 <div id="countdown-${idx}" style={{fontSize:"12px", color:"#10b981", fontWeight:"700", textAlign:"center"}}>⏱️ ${timeRemaining} remaining</div></div>
-            :<button 
+            :statusText=="Completed"? <button 
             onClick={()=>{clickClaim(id)}}
-            id="claimRewardsBtn" style={{background:"linear-gradient(135deg,#10b981,#059669)", color:"white", border:"none", padding:"16px 32px", borderRadius:"12px", cursor:"pointer", fontSize:"clamp(16px,3.5vw,18px)", fontWeight:"900", boxShadow:"0 8px 20px rgba(16,185,129,0.4)", minWidth:"40px", marginBottom:"8px"}}>🎁 Claim All Rewards</button>  
+            id="claimRewardsBtn" style={{background:"linear-gradient(135deg,#10b981,#059669)", color:"white", border:"none", padding:"16px 32px", borderRadius:"12px", cursor:"pointer", fontSize:"clamp(16px,3.5vw,18px)", fontWeight:"900", boxShadow:"0 8px 20px rgba(16,185,129,0.4)", minWidth:"40px", marginBottom:"8px"}}>🎁 Claim All Rewards</button>: null  
         }
           <div style={{fontSize:"10px", opacity:"0.6"}}>Staked on: ${timestamp}</div></div>
     </div>
